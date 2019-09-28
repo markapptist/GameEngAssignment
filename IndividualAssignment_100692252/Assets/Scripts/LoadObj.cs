24166c0e112b4c35bc51b5b0cbaf6f1402ab7af3
Mark <mark@theapptist.com>
2019-09-28 01:03:59 -0400

    Assignment 1 Added

---
+++ IndividualAssignment_100692252/Assets/Scripts/LoadObj.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class LoadObj : MonoBehaviour
+{
+    const string DLL_NAME = "DLLPlugin";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+
+    [DllImport(DLL_NAME)]
+    private static extern void readX(float xP);
+
+    [DllImport(DLL_NAME)]
+    private static extern void readY(float yP);
+
+    [DllImport(DLL_NAME)]
+    private static extern void readZ(float zP);
+
+    [DllImport(DLL_NAME)]
+    private static extern float reX();
+
+    [DllImport(DLL_NAME)]
+    private static extern float reY();
+
+    [DllImport(DLL_NAME)]
+    private static extern float reZ();
+
+    [DllImport(DLL_NAME)]
+    private static extern void readPosition();
+    public void LoadPosition()
+    {
+        readPosition();
+        
+        Vector3 newposi = new Vector3(reX(), reY(), reZ());
+        float step = 1000 * Time.deltaTime;
+        GameObject.Find("Cube").transform.position = Vector3.MoveTowards(GameObject.Find("Cube").transform.position, newposi, step);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            LoadPosition();
+        }
+    }
+}
---
+++ IndividualAssignment_100692252/Assets/Scripts/SaveObj.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class SaveObj : MonoBehaviour
+{
+    const string DLL_NAME = "DLLPlugin";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+
+    [DllImport(DLL_NAME)]
+    private static extern void readX(float xP);
+
+    [DllImport(DLL_NAME)]
+    private static extern void readY(float yP);
+
+    [DllImport(DLL_NAME)]
+    private static extern void readZ(float zP);
+
+    [DllImport(DLL_NAME)]
+    private static extern void writePosition();
+    public void SavePosition()
+    {
+        readX(GameObject.Find("Cube").transform.position.x);
+        readY(GameObject.Find("Cube").transform.position.y);
+        readZ(GameObject.Find("Cube").transform.position.z);
+        writePosition();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            SavePosition();
+        }
+    }
+}
